2f6c8657031f198797c79d85afdb9071058b3aac
Puskal Kafle <puskal@Puskal-2.local>
2015-07-18 18:30:26 +0545

    js

--- js/main.js
+++ js/main.js
@@ -49,11 +49,14 @@ jQuery(document).ready(function($) {
             } else {
                 currLink.removeClass("nav1hover");
             }
+            if($(window).width()> 900){
+            
              if(scrollPos > $(".section1").position().top){
             $(".nav1").css({ background:'rgba(0,0,0,.9)'});
         }else if(scrollPos <= $(".section1").position().top){
                 $(".nav1").css({ background:'rgba(0,0,0,.3)'});
         }
+            }
             
         });
     });
