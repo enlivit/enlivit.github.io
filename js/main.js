2f050e2cfb03e36f84877efa8dc7d72924f2f14e
Puskal Kafle <puskal@Puskal-2.local>
2015-09-02 21:57:30 +0545

    Tagline Smooth scroll banner btn, no letter spacing nav

--- js/main.js
+++ js/main.js
@@ -2,6 +2,20 @@ var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 new WOW().init();
 jQuery(document).ready(function($) {
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
     $(".mobile-menu-btn").click(function() {
         $(".body-wrap").toggleClass("body-left");
         $(".mobile-menu").toggleClass("menu-left");
